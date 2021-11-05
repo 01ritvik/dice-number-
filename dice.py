5ec54f2bf5f63dfc8e67ac4c1252a22150a0be47
01ritvik <60423384+01ritvik@users.noreply.github.com>
2021-11-06 00:19:59 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 import random
 
-a = random.randint(1,6)
-
-print("your dice number is: ", a)
+while True:
+    rolled_num = random.randint(1,6)
+    print("The dice rolled and you got: ", rolled_num)
+    input("Press any key to roll again.")
